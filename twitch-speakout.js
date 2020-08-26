6233be3014a75e68b8ea246a93cd1e8873d34bb9
Kimitaka Watanabe <kimitaka@hey.com>
2020-08-26 19:02:47 +0900

    Modified for Streamlabs Chatbox Widget modification

--- twitch-speakout.js
+++ twitch-speakout.js
@@ -1,10 +1,16 @@
-document.addEventListener('onEventReceived', function(obj) {
+window.addEventListener('message', function(e) {
+    const data = e.data || {};
+    if (data.type !== 'item')
+      return;
+    const message = data.message || {};
+    const obj = { detail: { command: message.command, body: message.body, from: message.from }};
+    
     const synth = window.speechSynthesis;
     if (obj.detail.command === 'PRIVMSG') {
-        var utter = new SpeechSynthesisUtterance(obj.detail.body);
-        utter.voice = synth.getVoices().filter(v => v.lang === 'ja-JP')[0];
-        utter.rate = 1;
-        utter.pitch = 1;
-        synth.speak(utter);
+      var utter = new SpeechSynthesisUtterance(obj.detail.body);
+      utter.voice = synth.getVoices().filter(v => v.lang === 'ja-JP')[0];
+      utter.rate = 1;
+      utter.pitch = 1;
+      synth.speak(utter);
     }
-});
+  });
